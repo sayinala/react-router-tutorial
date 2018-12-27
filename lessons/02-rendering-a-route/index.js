bc8c07b915f12821d983b656940c311a346f7ce4
sayinala <sayinala@tibco.com>
2018-12-27 08:25:57 -0800

    Developing the lesson 1 and lesson 2

--- lessons/02-rendering-a-route/index.js
+++ lessons/02-rendering-a-route/index.js
@@ -1,4 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import App from './modules/App'
-render(<App/>, document.getElementById('app'))
+import React from 'react';
+import { render } from 'react-dom';
+import App from './modules/App';
+import About from './modules/About';
+import Repos from './modules/Repos';
+import {Route, Router, hashHistroy} from 'react-router';
+
+render(
+    <Router history={hashHistroy}>
+        <Route path="/" component={App}></Route>
+        <Route path="/repos" component={Repos}></Route>
+        <Route path="/about" component={About}></Route>
+    </Router>, document.getElementById('app')
+)
